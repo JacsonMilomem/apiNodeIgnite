35d0156cbc55d50c1bcf9d8b4e0be4cf5e5a9711
JacsonMilomem <jmilomem@gmail.com>
2021-11-26 07:13:55 -0300

    subindo fonte para repositorio

---
+++ src/database/migrations/1637228568956-CreateCategories.ts
@@ -0,0 +1,35 @@
+import { Table, MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateCategories1637228568956 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from "tsyringe";
+
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+@injectable()
+class CreateSpecificationUseCase {
+  constructor(
+    @inject("SpecificationsRepository")
+    private specificationRepository: ISpecificationsRepository
+  ) {}
+
+  async execute({ name, description }: IRequest): Promise<void> {
+    const specificationAlreadyExist =
+      await this.specificationRepository.findByName(name);
+    console.log(specificationAlreadyExist);
+    if (specificationAlreadyExist) {
+      throw new Error("Specification Already Exists!");
+    }
+    await this.specificationRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationUseCase };
