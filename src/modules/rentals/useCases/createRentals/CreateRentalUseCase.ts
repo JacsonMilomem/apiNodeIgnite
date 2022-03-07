c5d8ed52eb7ca3b443d16c98f92ac99e0faa0157
JacsonMilomem <jmilomem@gmail.com>
2022-03-07 07:19:36 -0300

    Criando teste de rental

---
+++ src/modules/rentals/useCases/createRentals/CreateRentalUseCase.ts
@@ -0,0 +1,43 @@
+import { Rental } from "@module/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@module/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is Unavalible");
+    }
+
+    const user = await this.rentalsRepository.findOpenRentalByUser(user_id);
+
+    if (user) {
+      throw new AppError("There's a rental ins progress for user!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
