80f3bd8cfb878c14ab08eb8b98e53f7f88cdfd05
JacsonMilomem <jmilomem@gmail.com>
2022-03-18 06:39:19 -0300

    Lista Aluguel de carro

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateRenatalController } from "@module/rentals/useCases/createRentals/CreateRentalController";
 import { DevolutionRentalController } from "@module/rentals/useCases/devolutionRentals/DevolutionRentalController";
+import { ListRentalsByUserController } from "@module/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 
 import { ensureAuthenticated } from "../middlewares/ensureauthenticated";
 
@@ -9,12 +10,18 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRenatalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalController.handle
+);
+rentalRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
